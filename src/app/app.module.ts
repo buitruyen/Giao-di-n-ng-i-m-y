1261a76b1eb489618719f11d72b8194829e4f904
buitruyenpro <namtruyenpro95@gmail.com>
2019-04-23 14:35:55 +0700

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ElementsModule} from "./elements/elements.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
+        SharedModule,
         BrowserModule,
         AppRoutingModule,
         ElementsModule
