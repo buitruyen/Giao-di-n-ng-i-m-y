b64201eab76df0b18020206917493e314b1f9adf
buitruyenpro <namtruyenpro95@gmail.com>
2019-04-23 12:56:47 +0700

    ok

--- src/app/elements/elm-video/elm-video.component.ts
+++ src/app/elements/elm-video/elm-video.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
-	selector: '[app-elm-video]',
-	templateUrl: './elm-video.component.html'
+    selector: '[app-elm-video]',
+    templateUrl: './elm-video.component.html'
 })
 export class ElmVideoComponent implements OnInit {
-
-	ngOnInit() {
-	}
+    @Input("show-desc") showDesc: boolean = false;
+    ngOnInit() {
+    }
 }
