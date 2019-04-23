e6326556fcca0c15dc0cc3c802808dffbb4933bd
buitruyenpro <namtruyenpro95@gmail.com>
2019-04-23 23:03:05 +0700

    ok

--- src/app/admin/components/training.component.ts
+++ src/app/admin/components/training.component.ts
@@ -13,13 +13,26 @@ export class TrainingComponent implements OnInit {
     item: FirebaseObjectObservable<any>;
 
     constructor(db: AngularFireDatabase) {
+        //Hiện thị
         this.items = db.list('/items').valueChanges();
-        
+
         this.item = db.object('/items/course2').valueChanges();
         this.items.subscribe(valueOfItems => {
             console.log(valueOfItems);
-        })
+        });
 
+        //Xóa
+        // const promise = db.object('items/abc').remove();
+        // promise.then(_ => console.log('success'))
+        //     .catch(err => console.log(err, 'You dont have access!'));
+        //Save
+        const itemRefSave = db.object('items/abc');
+        itemRefSave.set({id:123, name: 'php'});
+
+        //Updating data
+        const itemRefUpdate = db.object('items/abc');
+        itemRefUpdate.update({ id: 150 });
+        
     }
 
     ngOnInit(): void {
