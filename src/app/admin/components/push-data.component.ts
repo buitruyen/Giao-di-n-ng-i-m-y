2bbb59c061fa0d504c016f6cb5bba2028785e0f1
buitruyenpro <namtruyenpro95@gmail.com>
2019-04-24 12:11:50 +0700

    ok

--- src/app/admin/components/push-data.component.ts
+++ src/app/admin/components/push-data.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase} from "@angular/fire/database";
 import {dbAds} from "../../../environments/db-ads";
 import {dbPlaylist} from "../../../environments/db-playlist";
+import any = jasmine.any;
 
 @Component({
     selector: 'app-admi-push-data',
@@ -20,7 +21,7 @@ export class PushDataComponent implements OnInit {
     }
 
     onPushData() {
-        // //Thêm dữ liệu
+        // //Thêm dữ liệu ads
         // const itemsRef = this._db.list('ads');
         // dbAds.ads.forEach((ad: any) => {
         //     itemsRef.push({
@@ -31,10 +32,36 @@ export class PushDataComponent implements OnInit {
         //     });
         // });
 
-        //Thêm dữ liệu
+        //Thêm dữ liệu cho playlist
         const itemsRef = this._db.list('playlist');
         dbPlaylist.playlist.forEach((playlist: any) => {
-            console.log(playlist);
+            itemsRef.push({
+                id: playlist.id,
+                channelId:playlist.channelId,
+                title: playlist.title,
+                slug: playlist.slug,
+                description: playlist.description,
+                thumbnails: playlist.thumbnails,
+            });
         });
+
+        // const itemsRef = this._db.list('playlist');
+        // var myArray = [];
+        // dbPlaylist.playlist.forEach((playlist: any) => {
+        //     myArray.push(playlist['items'])
+        // });
+        // myArray.forEach((item: any) => {
+        //     item.forEach((i: any)=>{
+        //         itemsRef.push({
+        //             id: i.id,
+        //             channelId: i.channelId,
+        //             title: i.title,
+        //             slug: i.slug,
+        //             description: i.description,
+        //             thumbnails: i.thumbnails,
+        //         });
+        //     })
+        // })
     }
+
 }
