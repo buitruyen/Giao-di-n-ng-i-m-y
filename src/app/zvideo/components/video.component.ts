6a34589f8fd2f44758a76cd2de2e3eed8b01ae8b
buitruyenpro <namtruyenpro95@gmail.com>
2019-04-25 21:08:48 +0700

    ok

--- src/app/zvideo/components/video.component.ts
+++ src/app/zvideo/components/video.component.ts
@@ -1,21 +1,24 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {AfterViewChecked, Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router, RoutesRecognized} from "@angular/router";
 import {VideoService} from "../../shared/services/video.service";
 import {Video} from "../../shared/defines/video.class";
 import {Subscription} from "rxjs";
+import * as $ from 'jquery';
+import {filter, pairwise} from "rxjs/operators";
+
 
 @Component({
     selector: 'app-zvideo-video',
     templateUrl: './../templates/video.component.html'
 })
-export class VideoComponent implements OnInit, OnDestroy {
+export class VideoComponent implements OnInit, OnDestroy ,AfterViewChecked{
     videoID: string = null;
     videoInfor: Video = null;
     items: Video[] = null;
     layoutDefault: string = 'grid';
     subscription: Subscription;
 
-    constructor(private _activatedRouterService: ActivatedRoute, private _videoService: VideoService) {
+    constructor(private _activatedRouterService: ActivatedRoute, private _videoService: VideoService,private _routeService:Router) {
     }
 
     ngOnInit() {
@@ -36,5 +39,16 @@ export class VideoComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
+    }
+
+    ngAfterViewChecked(): void {
+        this._routeService.events
+            .pipe(filter((e: any) => e instanceof RoutesRecognized),
+                pairwise()
+            ).subscribe((e: any) => {
+            $("html, body").stop().animate({scrollTop:0}, 1000, 'swing');
+        });
+       
+
     }
 }
